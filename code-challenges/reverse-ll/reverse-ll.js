724e2c7d12147e1d34d4252739ef0be783486cc1
dana-younis <younisdana7@gmail.com>
2021-07-07 20:30:41 +0300

    ?

---
+++ code-challenges/reverse-ll/reverse-ll.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Require the linked list implementation
+const LinkedList = require('../../code-challenges/linked-list/index');
+
+
+/*
+  Accept a linked list
+  Reveres it
+  Return it
+*/
+module.exports = function reverse(list) {
+
+  return true;
+
+};
